ff8b91729a7bca2aeba686178a4e8ce9fe90f7ce
Jabbernaut <jabbernaut4@gmail.com>
2022-07-10 20:35:39 -0400

    push 1

---
+++ test.js
@@ -0,0 +1,23 @@
+//the first alert
+alert("Hello, World!");
+
+//the second alert
+// alert("Hi again");
+
+// var myVar = 10;
+// myVar = myVar + 5; //add 5
+// myVar += 5; //shorthand for previous line
+// myVar = myVar - 5; //subtract 5
+// myVar -= 5; //shorthand for previous line
+// myvar /= 2; //divide by 2
+// myvar *= 2; //multiply by 2
+// myVar++; //add 1
+// myVar--; //subtract 1
+
+var myVar = "Shaun"
+document.write(myVar);
+console.log(myVar);
+document.write(10);
+console.log(50);
+console.log(20 * 5);
+document.write(20 / 5);
